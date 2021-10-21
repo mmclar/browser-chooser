a39a8bdac2c41cea95fba26199e6022bb32be257
Michael McLarnon <mmclar@gmail.com>
2021-10-21 08:51:17 -0400

    activate venv when installing

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 virtualenv venv
+source venv/bin/activate
 pip install -r requirements.txt
 sudo desktop-file-install ~/projects/browser-chooser/browser-chooser.desktop 
 sudo update-desktop-database 
