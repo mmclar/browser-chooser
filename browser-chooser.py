2f83bcd84e366ee39d0d9d64d384250aed2fe22c
Michael McLarnon <mmclar@gmail.com>
2023-01-29 12:20:42 -0500

    remove unused choices; make automatic selection when only one exists

--- browser-chooser.py
+++ browser-chooser.py
@@ -6,11 +6,24 @@ ESC = 27
 
 browsers = {
     'p': ('Firefox (personal)', ['firefox', '-P', 'mmclar', '{url}']),
-    'w': ('Firefox (work)', ['firefox', '-P', 'work', '{url}']),
-    'c': ('Chrome', ['google-chrome', '{url}']),
+#    'w': ('Firefox (work)', ['firefox', '-P', 'work', '{url}']),
+#    'c': ('Chrome', ['google-chrome', '{url}']),
 }
 
 url = sys.argv[1] if len(sys.argv) > 1 else ''
+
+def openByChar(c):
+    _, params = browsers[c]
+    subprocess.Popen(['nohup'] + [formatted for p in params if (formatted := p.format(url=url))],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    sys.exit()
+
+# If there's only one choice, open it
+if len(browsers) == 1:
+    openByChar(next(iter(browsers)))
+
 print(url)
 print('Choose which browser you want to use for this link:')
 for key, (name, _) in browsers.items():
@@ -18,14 +31,10 @@ for key, (name, _) in browsers.items():
 
 while True:
     try:
-        _, params = browsers[(c := readchar.readchar())]
-        subprocess.Popen(['nohup'] + [formatted for p in params if (formatted := p.format(url=url))],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT
-        )
-        break
+        openByChar(readchar.readchar())
     except KeyError:
         if ord(c) in (ESC,):
             print('Exiting')
             sys.exit(0)
-        print(f'Invalid choice. Please choose one of [{", ".join(browsers.keys())}]')
+    print(f'Invalid choice. Please choose one of [{", ".join(browsers.keys())}]')
+
